d4201857a4c3fee2cd73f799d5fbeee08ac09923
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-05 17:41:33 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 const App = () => {
     return (
         <div className='app'>
+            <Navbar />
             <Header />
             <About />
             <Work />
