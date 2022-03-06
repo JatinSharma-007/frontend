0a2ac2c43357052517c8a9adc06d27530a6eda32
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-06 18:48:51 +0530

    message

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-
+import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -28,13 +28,13 @@ const Header = () => (
                     <span>👋</span>
                     <div style={{ marginLeft: 20 }}>
                         <p className="p-text">Hello, I am</p>
-                        <h1 className="head-text">Jatin</h1>
+                        <h1 className="head-text">Micael</h1>
                     </div>
                 </div>
 
                 <div className="tag-cmp app__flex">
-                    <p className="p-text">FULL STACK DEVELOPER</p>
-                    <p className="p-text">UI/ UX DEVELOPER</p>
+                    <p className="p-text">Web Developer</p>
+                    <p className="p-text">Freelancer</p>
                 </div>
             </div>
         </motion.div>
@@ -68,4 +68,4 @@ const Header = () => (
     </div>
 );
 
-export default Header
+export default AppWrap(Header, 'home');
--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -1,51 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
 import { urlFor, client } from '../../client';
 
-// const abouts = [
-//     { title: 'FrontEnd Development', description: 'I am a good web developer', imgUrl: images.about01 },
-//     { title: 'BackEnd Development', description: 'I am a good web developer', imgUrl: images.about02 },
-//     { title: 'Web3 Apps', description: 'I am a good web developer', imgUrl: images.about03 },
-//     { title: 'UI/ UX Development', description: 'I am a good web developer', imgUrl: images.about04 },
-
-// ]
-
 const About = () => {
     const [abouts, setAbouts] = useState([]);
-    useEffect(() => {
-        const query = '*[_type == "Abouts"]';
 
-        client.fetch(query)
-            .then((data) => {
-                setAbouts(data)
-            })
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+
+        client.fetch(query).then((data) => {
+            setAbouts(data);
+        });
     }, []);
+
     return (
         <>
-            <br />
-            <h2 className='head-text'>Full Stack <span> Developer </span> and <span> Designer.</span> </h2>
+            <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
-            <div className='app__profiles'>
+            <div className="app__profiles">
                 {abouts.map((about, index) => (
                     <motion.div
                         whileInView={{ opacity: 1 }}
                         whileHover={{ scale: 1.1 }}
                         transition={{ duration: 0.5, type: 'tween' }}
-                        className='app__profile-item'
+                        className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={urlFor(about.imgUrl)} alt='about.title' />
-                        <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
-                        <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
-
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
+                        <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+                        <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
                     </motion.div>
                 ))}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default About;
+export default AppWrap(
+    MotionWrap(About, 'app__about'),
+    'about',
+    'app__whitebg',
+);
